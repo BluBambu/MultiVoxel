4024a7b64fb420276ebf793e1761443ece67a829
BluBambu <wenjieshi1@gmail.com>
2015-11-15 15:35:22 -0800

    Added in adding voxel animations

--- multivoxel/Assets/Scripts/VoxelModel.cs
+++ multivoxel/Assets/Scripts/VoxelModel.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 // Represents the voxel model in the editor
 [RequireComponent(typeof(VoxelModelRenderer))]
-public class VoxelModel : MonoBehaviour 
+public class VoxelModel : MonoBehaviour
 {
+    private const float VoxelTransitionTime = 5f;
+
 	private VoxelModelRenderer _voxelRenderer;
 	private VoxelData _voxelData;
 
@@ -23,13 +26,29 @@ public class VoxelModel : MonoBehaviour
 
     public void AddVoxel(Vector3 pos, Color color)
     {
-        _voxelData.AddVoxel(Utils.RoundVector3(pos), color);
-        _voxelRenderer.RenderMesh(_voxelData);
+        StartCoroutine(AddVoxelCor(pos, color));
     } 
 
     public void RemoveVoxel(Vector3 pos)
     {
         _voxelData.RemoveVoxel(Utils.RoundVector3(pos));
+        _voxelRenderer.RenderMesh(_voxelData);
+    }
+
+    // TODO: Change the shader
+    private IEnumerator AddVoxelCor(Vector3 pos, Color color)
+    {
+        Transform tempVoxel = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+        tempVoxel.position = pos;
+        tempVoxel.localScale = Vector3.zero;
+        tempVoxel.GetComponent<MeshRenderer>().material.color = color;
+        while (tempVoxel.localScale.x < .95f)
+        {
+            tempVoxel.localScale = Vector3.Lerp(tempVoxel.localScale, Vector3.one, .1f);
+            yield return null;
+        }
+        Destroy(tempVoxel.gameObject);
+        _voxelData.AddVoxel(Utils.RoundVector3(pos), color);
         _voxelRenderer.RenderMesh(_voxelData);
     }
  }
