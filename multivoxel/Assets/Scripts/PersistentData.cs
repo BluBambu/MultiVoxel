e18fd2bbefbada078ee76c189b0cdba2d99f6971
Wenjie Shi <wenjieshi1@gmail.com>
2015-12-05 14:57:03 -0800

    Hooked up persistent data to save the load path

--- multivoxel/Assets/Scripts/PersistentData.cs
+++ multivoxel/Assets/Scripts/PersistentData.cs
@@ -2,5 +2,16 @@
 
 public class PersistentData : MonoBehaviour
 {
-    public string SavePath;
+    public static PersistentData Instance
+    {
+        get { return _instance ?? (_instance = FindObjectOfType<PersistentData>()); }
+    }
+    private static PersistentData _instance; // Backing field, don't modify
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public string LoadPath;
 }
