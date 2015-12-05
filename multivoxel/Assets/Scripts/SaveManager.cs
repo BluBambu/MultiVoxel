a959c12b8c6b80204b061bc2a318c8ff63c09acf
gulati@uw.edu <ggulati@live.com>
2015-12-05 14:16:15 -0800

    Save functionality complete; also includes OBJ export

---
+++ multivoxel/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class SaveManager : MonoBehaviour {
+    public Button _saveButton;
+    public InputField _filepathInputField;
+    private VoxelController _voxelController;
+
+	// Use this for initialization
+	void Start () {
+        _voxelController = FindObjectOfType<VoxelController>();
+        _saveButton.onClick.AddListener(() =>
+        {
+            try
+            {
+                print("Button clicked!");
+                _voxelController.SaveToFile(_filepathInputField.text);
+            }
+            catch (Exception e)
+            {
+                // TODO: print error to user
+                Debug.Log("Could not save to '" + _filepathInputField.text + "' due to " + e.ToString());
+            }
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
