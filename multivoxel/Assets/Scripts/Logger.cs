e2ee4936fa4682b06e0ef6c14ba51b78ca97e412
Kevin Vu <kevin.m.vu@gmail.com>
2015-12-05 13:43:14 -0800

    Add Logger class

---
+++ multivoxel/Assets/Scripts/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+public class Logger {
+
+	private StreamWriter _writer;
+
+	public Logger(string filepath) {
+		_writer = new StreamWriter(File.Create (filepath));
+	}
+
+	public void Log(string message) {
+		String s = String.Format ("{0}: {1}", DateTime.UtcNow.ToString ("MM/dd/yy HH:mm:ss.fff"), message);
+		_writer.WriteLine (s);
+		_writer.Flush ();
+	}
+}
