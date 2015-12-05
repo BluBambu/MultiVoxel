8c9d6341c31be6275b1a2e0ea4ae13be65584aab
Kevin Vu <kevin.m.vu@gmail.com>
2015-12-05 13:09:52 -0800

    Factor out Unity scripting from Client.cs and Server.cs to NetworkManager.cs

---
+++ multivoxel/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (Config.HAS_SERVER) {
+			Concurrency.StartThread(() => Server.Start(Config.SERVER_PORT), "server thread");
+		}
+		Concurrency.StartThread(() => Client.Start(Config.SERVER_ADDRESS, Config.SERVER_PORT), "client thread");
+	}
+}
