c3e1587f42068c20dfc1f8db290b5df54030815b
Wenjie Shi <wenjieshi1@gmail.com>
2015-12-05 14:47:15 -0800

    Added a string field to PersistentData

--- multivoxel/Assets/Scripts/PersistentData.cs
+++ multivoxel/Assets/Scripts/PersistentData.cs
@@ -1,15 +1,6 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class PersistentData : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class PersistentData : MonoBehaviour
+{
+    public string SavePath;
 }
