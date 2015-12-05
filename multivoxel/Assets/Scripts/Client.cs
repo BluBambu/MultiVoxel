077845d7fde2eee7fa265231bfef4edc118e3212
Kevin Vu <kevin.m.vu@gmail.com>
2015-12-04 18:58:50 -0800

    Refactor network components. Client.cs and Server.cs are now decoupled from UnityEngine code.

--- multivoxel/Assets/Scripts/Client.cs
+++ multivoxel/Assets/Scripts/Client.cs
@@ -1,33 +1,51 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Net.Sockets;
 
 /*
- * Client provides an API to send and receive messages from the server.
- * 
- * This script starts a client that tries to connect to a server with parameters specified in Config.cs.
- * 
- * Game clients should communicate with the server only through static methods defined here.
+ * Client provides static methods to
+ *   Connect to a server
+ *   Send objects to the server
+ *   Receive objects from the server
+ *
+ * Game clients should communicate with the server only through methods defined here.
  */
-public class Client : MonoBehaviour {
+public static class Client {
 	private static Socket _socket;
-	private static IDictionary<System.Type, Queue<object>> _queues = new Dictionary<System.Type, Queue<object>>();
-	
-	void Start () {
+	private static Queue<object> _sendQueue = new Queue<object>();
+	private static IDictionary<System.Type, Queue<object>> _receiveQueues = new Dictionary<System.Type, Queue<object>>();
+
+	public static void Start(string serverAddress, int serverPort) {
 		_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-		_socket.Connect (Config.SERVER_ADDRESS, Config.SERVER_PORT);
-		Concurrency.StartThread (RunClient, "client main thread");
+		_socket.Connect (serverAddress, serverPort);
+		Concurrency.StartThread (Sender, "client sender");
+		Concurrency.StartThread (Receiver, "client receiver");
 	}
 
-	private static void RunClient() {
+	private static void Sender() {
+		while (true) {
+			bool hasObj = false;
+			object obj = null;
+			lock (_sendQueue) {
+				if (_sendQueue.Count > 0) {
+					hasObj = true;
+					obj = _sendQueue.Dequeue();
+				}
+			}
+			if (hasObj) {
+				Protocol.Send(_socket, obj);
+			}
+		}
+	}
+
+	private static void Receiver() {
 		while (true) {
 			object obj = Protocol.Receive(_socket);
 			System.Type type = obj.GetType();
-			lock (_queues) {
+			lock (_receiveQueues) {
 				Queue<object> queue;
-				if (!_queues.TryGetValue(type, out queue)) {
+				if (!_receiveQueues.TryGetValue(type, out queue)) {
 					queue = new Queue<object>();
-					_queues.Add (type, queue);
+					_receiveQueues.Add (type, queue);
 				}
 				queue.Enqueue(obj);
 			}
@@ -36,23 +54,24 @@ public class Client : MonoBehaviour {
 
 	// Send an object to the server.
 	// Requires that obj is serializable.
-	// TODO(kvu787): Non-blocking (does no I/O operations).
 	public static void Send(object obj) {
-		Protocol.Send (_socket, obj);
+		lock (_sendQueue) {
+			_sendQueue.Enqueue(obj);
+		}
 	}
 
 	/*
 	 * Retrieve the next object of type T sent from the server, if available.
 	 * Non-blocking (does no I/O operations).
-	 * 
+	 *
 	 * The underlying model is a set of queues, each containing objects of a single C# type.
 	 * For example, given the current queue state:
-	 * 
+	 *
 	 *   ChatMessage  : ["hello", "goodbye"]
 	 *   VoxelCommand : [ cmd1, cmd2 ]
-	 * 
+	 *
 	 * The output for several calls to TryReceive follow:
-	 * 
+	 *
 	 *   TryReceive<VoxelCommand> => cmd1,    true
 	 *   TryReceive<ChatMessage>  => "hello", true
 	 *   TryReceive<VoxelCommand> => cmd2,    true
@@ -60,9 +79,9 @@ public class Client : MonoBehaviour {
 	 *   TryReceive<VoxelDatat>   => ?,       false
 	 */
 	public static bool TryReceive<T>(out T t) {
-		lock (_queues) {
+		lock (_receiveQueues) {
 			Queue<object> queue;
-			if (_queues.TryGetValue(typeof(T), out queue)) {
+			if (_receiveQueues.TryGetValue(typeof(T), out queue)) {
 				if (queue.Count > 0) {
 					t = (T) queue.Dequeue();
 					return true;
@@ -73,3 +92,4 @@ public class Client : MonoBehaviour {
 		}
 	}
 }
+
